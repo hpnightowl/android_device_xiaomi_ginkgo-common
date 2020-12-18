887bd2f46b311a34d578d063c25074ab1c340b70
HARSH <iamhp2k@gmail.com>
2020-12-18 20:22:12 +0530

    trinket-common: build mock power.stats for BatteryStatsService
    *E BatteryStatsService: Unable to load Power Hal or power.stats HAL

--- trinket.mk
+++ trinket.mk
@@ -291,8 +291,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
+     
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vndk/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
